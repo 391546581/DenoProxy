a00e0ae862139220f69943582f55645d136310ba
blue <391546581@qq.com>
2025-09-23 09:05:16 +0800

    Create main.ts

---
+++ main.ts
@@ -0,0 +1,15 @@
+import { serve } from "https://deno.land/std@0.155.0/http/server.ts";
+
+async function handler(req: Request): Promise<Response> {
+  const url = new URL(req.url);
+  url.protocol = "https:"; // 传输协议
+  url.hostname = "example.com"; // 反代域名
+  url.port = "443"; // 访问端口
+  return await fetch(url.href, {
+    headers: req.headers,
+    method: req.method,
+    body: req.body,
+  });
+}
+
+serve(handler);
