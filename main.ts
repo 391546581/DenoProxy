e42e1ed22082c4f0742f09aaa458fa1e501333ac
blue <391546581@qq.com>
2025-09-23 09:05:40 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@ import { serve } from "https://deno.land/std@0.155.0/http/server.ts";
 async function handler(req: Request): Promise<Response> {
   const url = new URL(req.url);
   url.protocol = "https:"; // 传输协议
-  url.hostname = "example.com"; // 反代域名
+  url.hostname = "translate.googleapis.com"; // 反代域名
   url.port = "443"; // 访问端口
   return await fetch(url.href, {
     headers: req.headers,
